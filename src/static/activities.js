9917af95dcd102afccc26f87858cb1511129ff42
Cui1994 <269584357@qq.com>
2020-11-17 00:17:28 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -1076,6 +1076,20 @@ const activities = [
     "average_heartrate": null,
     "average_speed": 2.8797492078650606,
     "streak": 1
+  },
+  {
+    "run_id": 9223370431330930707,
+    "name": "run from keep",
+    "distance": 5175.762283463357,
+    "moving_time": "0:35:30",
+    "type": "Run",
+    "start_date": "2020-11-16 10:15:14",
+    "start_date_local": "2020-11-16 18:15:14",
+    "location_country": "{'latitude': 31.18409423828125, 'longitude': 121.60859347873264, 'country': '\u4e2d\u56fd', 'nationCode': '156', 'province': '\u4e0a\u6d77\u5e02', 'city': '\u4e0a\u6d77\u5e02', 'cityCode': '021'}",
+    "summary_polyline": "",
+    "average_heartrate": null,
+    "average_speed": 2.4299353443489937,
+    "streak": 1
   }
 ];
 
